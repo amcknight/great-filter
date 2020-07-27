1358887483196e8ccc1ca6495c68ad689b8d4631
Andrew McKnight <thedonk@gmail.com>
2020-07-27 15:19:04 -0700

    More tutorialing

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,10 +2,33 @@ module Lib
     ( someFunc
     ) where
 
-import Control.Monad.Bayes.Class
+import Control.Monad (join)
+import Control.Monad.Bayes.Class (MonadInfer, score, condition, uniform, uniformD)
+import Control.Monad.Bayes.Sampler (sampleIOfixed)
+import Control.Monad.Bayes.Traced (mh)
+import Control.Monad.Bayes.Weighted (prior)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = join $ putStrLn <$> show <$> sample
 
-model1 :: MonadSample m => m Bool
-model1 = uniformD [False, True]
+numSamples :: Int
+numSamples = 20
+
+sample :: IO [Bool]
+sample = sampleIOfixed $ prior $ mh numSamples model2
+
+--model1 :: MonadSample m => m Bool
+--model1 = uniformD [False, True]
+--
+model2 :: MonadInfer m => m Bool
+model2 = do
+    b <- uniformD [False, True]
+    score (if b then 1.0 else 0.0)
+    return b
+
+model3 :: MonadInfer m => m (Double, Double)
+model3 = do
+    b <- uniform (-1) 1
+    m <- uniform (-1) 1
+    condition $ (b-m) > 0
+    return (b, m)
